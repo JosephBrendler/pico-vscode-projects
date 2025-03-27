bd673b234fa238bb2ca429951b0cead9cb629ba9
JosephBrendler <joseph.brendler@googlemail.com>
2025-03-27 16:40:20 -0400

    s

--- HsyncInterrupt/HsyncInterrupt.c
+++ HsyncInterrupt/HsyncInterrupt.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/timer.h"
+#include "hardware/gpio.h"
 
-volatile bool alarm_flag = false;
+#define IRQ_PIN 15
 
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    alarm_flag = true;
-    return 0;
+volatile uint64_t lastInterruptTime = 0;
+volatile uint64_t timeBetweenInterrupts = 0;
+volatile uint64_t currentTime = 0;
+volatile uint32_t interrupt_count = 0;
+volatile bool interrupt_flag = false;
+
+void gpio_callback(uint gpio, uint32_t events) {
+    currentTime = time_us_64();
+    timeBetweenInterrupts = currentTime - lastInterruptTime;
+    lastInterruptTime = currentTime;
+    interrupt_flag = true;
+    interrupt_count++;
 }
-
-
 
 int main()
 {
     stdio_init_all();
 
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_us(1, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+    gpio_init(IRQ_PIN);
+    gpio_set_dir(IRQ_PIN, GPIO_IN);
+    gpio_pull_down(IRQ_PIN);
+    gpio_set_irq_enabled_with_callback(IRQ_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
 
-    while (true) {
-        if (alarm_flag) {
-            printf("Hello, alarmed world!\n");
-            alarm_flag = false;
+    int count = 0;
+    while (count++ < 10) {
+        printf("(%d) ", count);
+        if (interrupt_flag) {
+            printf("Hello, %dth interrupted world!\n", interrupt_count);
+            printf("  time between Hsync interrupts: %llu\n", timeBetweenInterrupts);
+            interrupt_flag = false;
         } else {
-            printf("Hello, un-alarmed world!\n");
+            printf("Hello, un-interrupted world!\n");
         }
         sleep_ms(1000);
     }
+    return 0;
 }
