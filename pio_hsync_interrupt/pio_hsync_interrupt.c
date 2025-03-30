bf36e3adc943e4e3c54595915f00087b5c4f2e48
JosephBrendler <joseph.brendler@googlemail.com>
2025-03-29 23:48:53 -0400

    progress on pio_hsync_interrupt

--- pio_hsync_interrupt/pio_hsync_interrupt.c
+++ pio_hsync_interrupt/pio_hsync_interrupt.c
@@ -17,11 +17,16 @@
 // Define the GPIO pin to monitor
 #define GPIO_PIN 15
 
+// debug variable for testing
+static uint verbosity = 3;
+
 // Define the PIO instance and state machine to use
 static PIO PIO_0;
 static uint SM;
+static uint PIO_IRQ;
+static uint pioNum = 0;   // use to select PIO and IRQ
 
-static uint64_t SM_CLK_FREQ = ( 25 * MHZ );
+static float SM_CLK_FREQ = ( 25 * MHZ );
 
 volatile bool hsync_irq_flag = false;
 volatile uint64_t interrupt_count = 0;
@@ -32,55 +37,110 @@ volatile bool interrupt_flag = false;
 
 // Interrupt handler function
 void hsync_pio_isr() {
+    // disable irqs while handling one
+    irq_set_enabled(PIO_IRQ, false);
+    sleep_ms(100);
     // degug: print irq status bits, confirm what irq got us here
-    printf("[IRQ] %d - ", PIO_0->irq);
+    if (verbosity >= 3) { printf("[IRQ] %d - ", PIO_0->irq); }
     // Clear the interrupt flag
     if (pio_interrupt_get(PIO_0, 0)) // returns TRUE if IRQ 0 is set
     {
-        printf("IRQ0 - Before clearing [INTR]: %x [IRQ]: %x; ", PIO_0->intr, PIO_0->irq);
+        if (verbosity >= 3) { printf("IRQ0 - Before clearing [INTR]: %x [IRQ]: %x; ", PIO_0->intr, PIO_0->irq); }
         pio_interrupt_clear(PIO_0, 0);
-        printf("After clearing [INTR]: %x [IRQ]: %x \n", PIO_0->intr, PIO_0->irq);
+        if (verbosity >= 3) { printf("After clearing [INTR]: %x [IRQ]: %x \n", PIO_0->intr, PIO_0->irq); }
     } else {
-        printf("got wrong interrupt signal [INTR]: %x [IRQ]: %x \n", PIO_0->intr, PIO_0->irq);
+        printf("Error: got wrong interrupt signal [INTR]: %x [IRQ]: %x \n", PIO_0->intr, PIO_0->irq);
     }
 
     currentTime = time_us_64();
+    printf("currentTime: %llu\n", currentTime);
+    
     timeBetweenInterrupts = currentTime - lastInterruptTime;
     lastInterruptTime = currentTime;
     interrupt_flag = true;
     interrupt_count++;
+    // re-enable irqs after handling one
+    irq_set_enabled(PIO_IRQ, true);
+    sleep_ms(100);
 }
 
 int main() {
     stdio_init_all();
 
-    // Configure the GPIO pin as input with pull-down resistor
-    gpio_init(GPIO_PIN);
-    gpio_set_dir(GPIO_PIN, GPIO_IN);
-    gpio_pull_down(GPIO_PIN);
+    // move this to pio helper function?
+//    gpio_set_dir(GPIO_PIN, false); // set gpio as input for sync pin
+//    gpio_pull_down(GPIO_PIN);       // maybe make this pull up (since it drives low)
+//    gpio_pull_up(GPIO_PIN);       // maybe make this pull up (since it drives low)
+
+    printf("start program\n");
+
+    printf("assigning PIO and IRQ for pionum: %d\n", pioNum);
+    PIO_0 = pioNum ? pio1 : pio0;               // Selects the pio instance (0 or 1 for pioNUM)
+    PIO_IRQ = pioNum ? PIO1_IRQ_0 : PIO0_IRQ_0; // Selects the NVIC PIO_IRQ to use
+    printf("assigned PIO %d, SM IRQ 0, mapped to NVIC PIO_IRQ (0/1): %d\n", pioNum, PIO_IRQ);
 
     // Load PIO program to monitor rising edge
     uint offset = pio_add_program(PIO_0, &hsync_program); 
-    // claim pio0 sm0 as ours
-    hsync_pio_sm_claim(PIO_O, 0);
+    printf("loaded hsync_program and got offset: %d\n", offset);
+
+    // select the desired state machine clock frequency (2000 is about the lower limit)
+    printf("assigned SM_CLK_FREQ: %.4f\n", SM_CLK_FREQ);
+    float div = clock_get_hz(clk_sys) / SM_CLK_FREQ ;  // calculates the clock divider
+    printf("helper function will calculate divider div (clk_sys / SM_CLK_FREQ = %.4f)", div);
+ 
+    // assign hsync pin
+    printf("assigned Hsync GPIO_PIN: %d\n", GPIO_PIN);
+
+
+    // disable irqs while dpomg seti[]
+    irq_set_enabled(PIO_IRQ, false);
+    sleep_ms(100);
+
+    // examine existing SMs on this PIO
+    for (uint i = 0; i < 4; i++) {
+        if (pio_sm_is_claimed(PIO_0, i)){
+            // don't know why it's busy, but set it free
+            printf("SM: %d is claimed, trying to unclaim ...", i);
+            pio_sm_unclaim(PIO_0, i);
+            printf("now unclaimed.\n");
+        } else {
+            printf("checked SM: %d, (uncliamed)\n", i);
+        }
+    }
+    printf("now trying to claim an unused SM ... ");
+    SM = pio_claim_unused_sm(PIO_0, true);
+    printf("claimed SM: %d\n", SM);
     // initialize hsync program on sm
-    hsync_program_init(PIO_O, 0, offset, GPIO_PIN, SM_CLK_FREQ);
+    hsync_program_init(PIO_0, SM, offset, GPIO_PIN, SM_CLK_FREQ);
+    printf("initialized hsync_program on SM: %d, at offset %d, GPIO_PIN: %d, and SM_CLK_FREQ: %d\n", SM, offset, GPIO_PIN, SM_CLK_FREQ);
 
     // enables IRQ for the statemachine - setting IRQ0_INTE - interrupt enable register
     // pio_set_irq0_source_enabled(PIO_O, pis_interrupt0, true); // sets NVIC IRQ0
-    pio_set_irq0_source_enabled(PIO_0, SM, true);
+    // enumeration is at sdk ref p. 215
+    pio_set_irq0_source_enabled(PIO_0, pis_interrupt0, true);
+    printf("enabled irq0_source on pis_interrupt0: %d\n", pis_interrupt0);
 
     // Set up and enable the interrupt handler
-    irq_set_exclusive_handler(PIO0_IRQ_0, &hsync_pio_isr);
-    irq_set_enabled(PIO0_IRQ_0, true);
+    irq_set_exclusive_handler(PIO_IRQ, &hsync_pio_isr);
+    printf("irq_set_exclusive_handler done for PIO_IRQ: %d, &hsync_pio_isr\n", PIO_IRQ);
 
-    while (1) {
-        // Your main program logic here
+    // re-enable irqs after setup
+    irq_set_enabled(PIO_IRQ, true);
+    sleep_ms(100);
+    printf("irq_set_enabled for PIO_IRQ %d\n", PIO_IRQ);
+    printf("Done setup\n\n");
+
+    int count = 0;
+    while (count++ < 20) {
+        printf("(%d) ", count);
         if (interrupt_flag) {
             printf("Hello, %dth interrupted world!\n", interrupt_count);
             printf("  time between Hsync interrupts: %llu\n", timeBetweenInterrupts);
             interrupt_flag = false;
+        } else {
+            printf("Hello, un-interrupted world!\n");
         }
         sleep_ms(1000);
     }
+    return 0;
 }
